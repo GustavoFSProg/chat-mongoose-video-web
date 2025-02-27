b1f3a588ae0a641d9a288ac5d771f033af65b359
GustavoFSProg <gustavo.prog40@gmail.com>
2025-02-27 10:22:31 -0300

    novo layout

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,67 +1,92 @@
 import NavBar from "../../components/Navbar/Navbar"
 import reactImage from '../../assets/react.svg'
+import {Camera} from 'lucide-react'
+import userContext from "../../Contexts/userContext";
+import { useContext } from "react";
+  
+
 
 function Profile() {
+  const { user, isSignup } = useContext(userContext);
 
   return (
     <>
     <NavBar />
-    <br/>
-    <br/>
-    <br/>
-    <div className="flex w-full items-center flex-col justify-center h-full">
+    <div className="h-screen pt-20">
             
 
-         <div className="flex w-[34%]
-         justify-center
-         text-[20px]
-          rounded-[15px]
+         <div className="max-w-2xl mx-auto p-4 py-8">
          
-        h-[450px]
-          bg-[#251a0e]">
-         
+          <div className="bg-[#251a0e] h-[600px] rounded-xl p-6 space-y-8">
+          <div className="text-center">
+          <h2 className="text-2xl font-semibold">Profile</h2>
+          <p className=" mt-2 text-[15px]">
+             <span className="mr-[8px]">
+               Olá, 
+             </span>
+             
+              {user.name}
+            
+            </p>
+          </div>
+        <div className="flex flex-col items-center gap-4 mt-[30px]"> 
+          <div className="relative">
+          <img src={user.profilePic || reactImage}
+           className="size-32 rounded-full object-cover border-45" alt="perfil"/>
+           <label htmlFor="avatar-upload"
+           className={`
+            absolute bottom-0 right-0 bg-base-content 
+            hover:scale-105 p-2 roundend-full cursor-pointer 
+            transition-all duration-200
+
+
+            `}  
+           >
+
+
+          <Camera className="w-5 h-5 text-base-200"/>
+          
+            <input type="file" id="avatar-upload"
+            accept="image/*"
+            // onChange={handleImageUpload}
+            placeholder="" className="hidden"
+            // disabled={isUpdatingProfile}
+            />
+           </label>
+          </div>
           <div>
-          <h2 className="text-center mt-5">Profile</h2>
-          <p className="text-[13px] mt-2">Your Profile Information</p>
-          <img src={reactImage} className="mt-7 ml-4" width="120" alt="perfil"/>
-          </div>
-          </div>
-          <div className="-mt-[180px] ">
-            <p className="text-[13px]  ml-1">
+
+          <p className="text-[13px]  ml-1">
               Nome Completo
             </p>
-            <input placeholder="Nome" className="w-[400px]
-            mt-[4px]
-            p-3
-            text-[14px]
-            rounded-[8px] h-10"/>
+            <input type="text" className="w-[400px] 
+             p-3
+             text-[14px]
+             mt-[4px]  rounded-[8px] h-10"/>
+             </div>
+             <div>
+
             <p className="text-[13px] ml-1 mt-[15px]">
              Email
             </p>
             <input 
             placeholder="Email"
             className="w-[400px] 
-             p-3
+            p-3
             text-[14px]
             mt-[4px]  rounded-[8px] h-10" />
+            </div>
           </div>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
+          </div> 
+
+         
           <br/>
           <br/>
           <br/>
           <br/>
           </div> 
+          </div>
+
             </>
   )
 }
