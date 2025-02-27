3c7e4869be4753b8560f7d3db7bda00b7fed8133
GustavoFSProg <gustavo.prog40@gmail.com>
2025-02-27 10:51:11 -0300

    que massa boteia foto

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,13 +1,34 @@
 import NavBar from "../../components/Navbar/Navbar"
-import reactImage from '../../assets/react.svg'
+import reactImage from '../../assets/avatar.jpeg'
 import {Camera} from 'lucide-react'
 import userContext from "../../Contexts/userContext";
-import { useContext } from "react";
+import { useContext, useState } from "react";
   
 
 
 function Profile() {
   const { user, isSignup } = useContext(userContext);
+  const [selectedImg, setSelectedImg] = useState(null)
+
+ async function handleImageUpload(e){
+    const file = e.target.files[0]
+
+    console.log(`File: ${file}`)
+
+    if(!file)return
+
+    const reader = new FileReader()
+
+    reader.readAsDataURL(file)
+
+    reader.onload = async () => {
+      const base64Image = reader.result
+
+      setSelectedImg(base64Image)
+
+      await updateProfile({profilePic: base64Image})
+    }
+  }
 
   return (
     <>
@@ -31,8 +52,8 @@ function Profile() {
           </div>
         <div className="flex flex-col items-center gap-4 mt-[30px]"> 
           <div className="relative">
-          <img src={user.profilePic || reactImage}
-           className="size-32 rounded-full object-cover border-45" alt="perfil"/>
+          <img src={selectedImg ||  user.profilePic || reactImage}
+           className="size-38 rounded-full object-cover border-3" alt="perfil"/>
            <label htmlFor="avatar-upload"
            className={`
             absolute bottom-0 right-0 bg-base-content 
@@ -47,8 +68,8 @@ function Profile() {
           <Camera className="w-5 h-5 text-base-200"/>
           
             <input type="file" id="avatar-upload"
-            accept="image/*"
-            // onChange={handleImageUpload}
+            // accept="image/*"
+            onChange={handleImageUpload}
             placeholder="" className="hidden"
             // disabled={isUpdatingProfile}
             />
@@ -56,7 +77,7 @@ function Profile() {
           </div>
           <div>
 
-          <p className="text-[13px]  ml-1">
+          <p className="text-[13px] mt-10 ml-1">
               Nome Completo
             </p>
             <input type="text" className="w-[400px] 
@@ -66,7 +87,7 @@ function Profile() {
              </div>
              <div>
 
-            <p className="text-[13px] ml-1 mt-[15px]">
+            <p className="text-[13px] ml-1 mt-[7px]">
              Email
             </p>
             <input 
